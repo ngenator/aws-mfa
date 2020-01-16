4efee2bf237e46b22644c65de6745e12ac77a9ed
Nick Silverman <nsilverman@ruelala.com>
2020-01-16 12:00:13 -0500

    point at ruelala repo

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Daniel Ng <dan@ngenator.com>
+// Copyright © 2018 Daniel Ng <dan@RueLaLa.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws/external"
-	"github.com/ngenator/aws-mfa/mfa"
+	"github.com/RueLaLa/aws-mfa/mfa"
 	"github.com/spf13/cobra"
 )
 
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Daniel Ng <dan@ngenator.com>
+// Copyright © 2018 Daniel Ng <dan@RueLaLa.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@
 
 package main
 
-import "github.com/ngenator/aws-mfa/cmd"
+import "github.com/RueLaLa/aws-mfa/cmd"
 
 // Version gets set a build time
 var Version string
